accea461e035f698c2e10aece53a34e9da8cb649
Chubinbmt0202 <votrantrunganh@gmail.com>
2025-10-26 13:08:40 +0700

    feat: analytics GA4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 // src/App.jsx
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
+import { useLocation } from 'react-router-dom';
 import './App.css';
+import ReactGA from 'react-ga4';
 
 // Import các trang của bạn
 import TeacherDashboard from './page/Teacher/TeacherDashboard';
 import StudentWorkspace from './page/StudentWorkspace';
+
+const TRACKING_ID = "G-BYBSLN2K1M"; // Thay thế bằng Mã GA4 của bạn
+ReactGA.initialize(TRACKING_ID);
+
+const TrackingComponent = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Gửi sự kiện Page View đến GA4 mỗi khi location (URL) thay đổi
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search
+    });
+  }, [location]);
+
+  return null; // Component này không hiển thị gì, chỉ dùng để theo dõi
+};
 
 const { Header } = Layout;
 
@@ -15,6 +34,7 @@ function App() {
 
   return (
     <Layout>
+      <TrackingComponent />
       < Routes >
         <Route path="/teacher" element={<TeacherDashboard />} />
         <Route path="/student" element={<StudentWorkspace />} />
